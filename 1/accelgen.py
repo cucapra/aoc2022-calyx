2a5873ab735a7f7cb05d3a4ad76e6bb06c54a0f2
Adrian Sampson <adrian@radbox.org>
2022-12-03 11:33:53 -0800

    First stab at a top-k component

--- 1/accelgen.py
+++ 1/accelgen.py
@@ -1,4 +1,4 @@
-from calyx.builder import Builder, while_, if_
+from calyx.builder import Builder, while_, if_, const
 from calyx import py_ast as ast
 
 WIDTH = 32
@@ -131,7 +131,106 @@ def build():
         finish,
     ]
 
+    build_topk(prog, 3)
     return prog.program
+
+
+def build_topk(prog: Builder, k: int):
+    """Build a component that tracks the largest K values it sees.
+
+    The strategy is that we keep the current "running" top K in K
+    registers (unordered). If the new value is bigger than the smallest
+    of these registers, we drop that smallest value and put the new
+    value in its place. This strategy avoid the need to do sequential
+    comparisons, or to keep a sorted list, but it does require some
+    "reduction" logic to find the smallest value every time. This is
+    probably acceptable for small K and admits reasonable parallelism;
+    for larger K, you might want to store state about the order of the
+    current top K.
+    """
+    topk = prog.component(f"top{k}")
+
+    # You invoke the component with a new value to "push" into the set,
+    # and you get the sum of the top K values you have ever pushed in
+    # the past.
+    topk.input("value", WIDTH)
+    topk.output("total", WIDTH)
+
+    # We keep track of the top K values in K registers.
+    regs = [
+        topk.reg(f"reg{i}", WIDTH)
+        for i in range(k)
+    ]
+
+    # Continuously produce the sum of these registers. This could be a
+    # reduction tree, but for now it's just a reduction "stick."
+    with topk.continuous:
+        last_add = None
+        for i in range(1, k):
+            add = topk.add(f"sum{i}", WIDTH)
+            if last_add:
+                add.left = last_add.out
+            else:
+                add.left = regs[0].out
+            add.right = regs[i].out
+            last_add = add
+        topk.this().total = last_add.out
+
+    # Similarly, continuously compute the min and argmin of all our
+    # current values. There's a chance it would be better to wrap this
+    # up in a `comb group`, but it's not clear exactly where we would
+    # `with` it.
+    idx_width = k.bit_length()
+    with topk.continuous:
+        last_val = None
+        last_idx = None
+        for i in range(1, k):
+            left_val = last_val.out if last_val else regs[0].out
+            left_idx = last_idx.out if last_idx else 0
+
+            # Compare with the next register.
+            lt = topk.cell(f"lt{i}",
+                           ast.Stdlib().op("lt", WIDTH, signed=False))
+            lt.left = left_val
+            lt.right = regs[i].out
+
+            # Produce the resulting min and argmin.
+            val = topk.cell(f"val{i}",
+                            ast.Stdlib().op("wire", WIDTH, signed=False))
+            idx = topk.cell(f"idx{i}",
+                            ast.Stdlib().op("wire", idx_width, signed=False))
+            val.in_ = lt.out @ left_val
+            val.in_ = ~lt.out @ regs[i].out
+            idx.in_ = lt.out @ left_idx
+            idx.in_ = ~lt.out @ i
+
+            # Record the current wires for the next comparison.
+            last_val = val
+            last_idx = idx
+    min_val = last_val
+    min_idx = last_idx
+
+    # Check whether the input value is bigger than the smallest stored
+    # value.
+    gt = topk.cell("gt", ast.Stdlib().op("gt", WIDTH, signed=False))
+    with topk.comb_group("check") as check:
+        gt.left = topk.this().value
+        gt.right = min_val.out
+
+    # Replace the minimum value. Because we only have the index of the
+    # register we need, we need a bunch of conditional logic to write
+    # into the correct register.
+    with topk.group("upd") as upd:
+        for i in range(k):
+            const_i = const(idx_width, i)
+            regs[i].write_en = (min_idx.out == const_i) @ 1
+            regs[i].write_en = ~(min_idx.out == const_i) @ 0
+            regs[i].in_ = (min_idx.out == const_i) @ topk.this().value
+
+    # The control program.
+    topk.control += if_(gt.out, check, upd)
+
+    return topk
 
 
 if __name__ == '__main__':

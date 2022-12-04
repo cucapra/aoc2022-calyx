1543fc905167e0a914daae8e724eb02877cf3117
Adrian Sampson <adrian@radbox.org>
2022-12-04 14:01:04 -0800

    Generate scoring LUTs

--- 2/convert.py
+++ 2/convert.py
@@ -1,24 +1,47 @@
-"""Convert the strategy guide to numeric memories.
+"""Generate memories for the strategy guide and score tables.
 
 We encode Rock (A & X), Paper (B & Y), and Scissors (C & Z) into 2-bit
 numbers (0, 1, and 2). Then there are just two memories of equal length:
 "them" moves and "us" moves.
+
+We also include look-up tables for two scoring functions: one for the
+shape (indexed by the raw, two-bit rock/paper/scissors value) and one
+for the outcome (indexed by the 4-bit *concatenated pair* of "their"
+move and "our" move). It's possible this could be a lot more efficient
+if we instructed the generated Verilog that these are ROMs, to be baked
+into the design, rather than actual writable RAMs---but that's for
+another day.
 """
 import sys
 import json
 
 MAX_SIZE = 4096
 WIDTH = 32
+
+ROCK = 0
+PAPER = 1
+SCISSORS = 2
+
 THEM_NUMS = {
-    "A": 0,
-    "B": 1,
-    "C": 2,
+    "A": ROCK,
+    "B": PAPER,
+    "C": SCISSORS,
 }
 US_NUMS = {
-    "X": 0,
-    "Y": 1,
-    "Z": 2,
+    "X": ROCK,
+    "Y": PAPER,
+    "Z": SCISSORS,
 }
+
+WINS = {
+    (ROCK, SCISSORS),
+    (PAPER, ROCK),
+    (SCISSORS, PAPER),
+}
+SHAPE_SCORE = [1, 2, 3]
+LOSE_SCORE = 0
+DRAW_SCORE = 3
+WIN_SCORE = 6
 
 
 def convert(infile):
@@ -37,6 +60,7 @@ def convert(infile):
     padding = [0] * (MAX_SIZE - len(them_moves))
 
     return {
+        # Inputs.
         "them": {
             "data": them_moves + padding,
             "format": {
@@ -61,6 +85,8 @@ def convert(infile):
                 "width": MAX_SIZE.bit_length(),
             },
         },
+
+        # Output.
         "answer": {
             "data": [0],
             "format": {
@@ -69,7 +95,40 @@ def convert(infile):
                 "width": WIDTH,
             },
         },
+
+        # Look-up table ROMs.
+        "outcome_score": {
+            "data": gen_outcome_table(),
+            "format": {
+                "numeric_type": "bitnum",
+                "is_signed": False,
+                "width": WIDTH,
+            },
+        },
+        "shape_score": {
+            "data": SHAPE_SCORE,
+            "format": {
+                "numeric_type": "bitnum",
+                "is_signed": False,
+                "width": WIDTH,
+            },
+        },
     }
+
+
+def gen_outcome_table():
+    table = [0] * (2 ** 4)
+    for them in (ROCK, PAPER, SCISSORS):
+        for us in (ROCK, PAPER, SCISSORS):
+            idx = (them << 2) | us
+            if us == them:
+                score = DRAW_SCORE
+            elif (us, them) in WINS:
+                score = WIN_SCORE
+            else:
+                score = LOSE_SCORE
+            table[idx] = score
+    return table
 
 
 if __name__ == "__main__":
